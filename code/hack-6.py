91572ad8dc6687a8ba258786666d1ad6f711daa7
Vijay Kumar <vijaykumar@bravegnu.org>
2016-09-23 12:28:56 +0530

    Remove the code to print the hex version of the string.
    This is already shown in the slides.

--- code/hack-6.py
+++ code/hack-6.py
@@ -1,5 +1,4 @@
 import dis
-from binascii import hexlify
 
 def myfunc():
     return 6 / 2
@@ -9,12 +8,9 @@ def myfunc():
 co = myfunc.__code__
 
 bcode = co.co_code
-print hexlify(bcode)
 bcode = list(bcode)
-print [hexlify(c) for c in bcode]
 bcode[6] = "\x17"
 bcode = "".join(bcode)
-print hexlify(bcode)
 
 import new
 
